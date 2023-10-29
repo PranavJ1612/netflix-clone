9afe8d8036cadafb4c1ab4649b3cbe3102bbe13d
Pranav Jagdale <120732309+PranavJ1612@users.noreply.github.com>
2023-10-29 19:20:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmBob962fb1U9pwl-DtAHRbDRIOt7krlM",
-  authDomain: "netflix-clone-5cc1e.firebaseapp.com",
-  projectId: "netflix-clone-5cc1e",
-  storageBucket: "netflix-clone-5cc1e.appspot.com",
-  messagingSenderId: "199060446796",
-  appId: "1:199060446796:web:1f4352ba744c782cffdee9"
+  //fill ur firebase details
+  apiKey: ,
+  authDomain:,
+  projectId: ,
+  storageBucket:,
+  messagingSenderId:,
+  appId: "
 };
 
 
